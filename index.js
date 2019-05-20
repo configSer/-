719c0d48d78bee87da30e10921739c46c3ffdfb0
zhangyuhong <17610510720@163.com>
2019-05-20 18:24:45 +0800

    三级改二级

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 var selection = {
-  arr: ["province", "city", "area"],
-  obj: {province: "", city: "", area: ""},
+  arr: [
+    "province",
+    "city",
+  ],
+  obj: {
+    province: "",
+    city: "",
+  },
   init: function (el, callback, defaultValue) {
     let e = document.querySelector(el);
     selection.arr.forEach(item => {
@@ -29,7 +35,6 @@ var selection = {
       let code = evt.target.value;
       selection.obj.province = {[code]: data["100000"][code]};
       callback ? selection.city("#city", evt.target.value, callback) : selection.city("#city", evt.target.value);
-      selection.area("#area")
     }
   },
   city: function (el, provinceCode, callback) {
@@ -39,38 +44,35 @@ var selection = {
     o.innerHTML = "请选择市";
     a.appendChild(o);
     let obj = data[provinceCode];
-    for (let temp in obj) {
-      let opt = document.createElement("option");
-      opt.innerHTML = obj[temp];
-      opt['value'] = temp;
-      a.appendChild(opt);
-    }
-    a.onchange = function (evt) {
-      evt.preventDefault();
-      let code = evt.target.value;
-      selection.obj.city = {[code]: data[provinceCode][code]};
-      callback ? selection.area("#area", evt.target.value, callback) : selection.area("#area", evt.target.value)
-    }
-  },
-  
-  area: function (el, cityCode, callback) {
-    let a = document.querySelector(el);
-    a.innerHTML = "";
-    let o = document.createElement("option");
-    o.innerHTML = "请选择区/县";
-    a.appendChild(o);
-    let obj = data[cityCode];
-    for (let temp in obj) {
-      let opt = document.createElement("option");
-      opt.innerHTML = obj[temp];
-      opt['value'] = temp;
-      a.appendChild(opt);
-    }
-    a.onchange = function (evt) {
-      evt.preventDefault();
-      let code = evt.target.value;
-      selection.obj.area = {[code]: data[cityCode][code]};
-      callback && callback(selection.obj)
+    if (Object.keys(obj).length === 1) {
+      o.innerHTML = "请选择区/县";
+      let d = data[Object.keys(obj)[0]];
+      for (let temp in d) {
+        let opt = document.createElement("option");
+        opt.innerHTML = d[temp];
+        opt['value'] = temp;
+        a.appendChild(opt);
+      }
+      a.onchange = function (evt) {
+        evt.preventDefault();
+        let code = evt.target.value;
+        selection.obj.city = {[code]: d[code]};
+        callback && callback(selection.obj)
+      }
+    } else {
+      o.innerHTML = "请选择市";
+      for (let temp in obj) {
+        let opt = document.createElement("option");
+        opt.innerHTML = obj[temp];
+        opt['value'] = temp;
+        a.appendChild(opt);
+      }
+      a.onchange = function (evt) {
+        evt.preventDefault();
+        let code = evt.target.value;
+        selection.obj.city = {[code]: data[provinceCode][code]};
+        callback && callback(selection.obj)
+      }
     }
   },
   
@@ -85,15 +87,8 @@ var selection = {
           Object.entries(data[item[0]]).forEach((temp, cityIndex) => {
             if (value.indexOf(temp[1]) !== -1) {
               document.querySelector("#city").querySelectorAll("option")[cityIndex + 1].selected = true;
-              selection.obj.city = {[temp[0]]:temp[1]};
-              selection.area("#area", temp[0], callback);
-              Object.entries(data[temp[0]]).forEach((demo, areaIndex) => {
-                if (value.indexOf(demo[1]) !== -1) {
-                  document.querySelector("#area").querySelectorAll("option")[areaIndex + 1].selected = true;
-                  selection.obj.area = {[demo[0]]:demo[1]};
-                  callback && callback(selection.obj)
-                }
-              })
+              selection.obj.city = {[temp[0]]: temp[1]};
+              callback && callback(selection.obj)
             }
           })
         }
